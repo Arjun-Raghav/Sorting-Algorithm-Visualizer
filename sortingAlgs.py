b08d81e13f8145cbc69850f466a6a5767f5094dd
Arjun-Raghav <87092161+Arjun-Raghav@users.noreply.github.com>
2022-07-05 02:35:04 +0530

    Update sortingAlgs.py

--- sortingAlgs.py
+++ sortingAlgs.py
@@ -4,17 +4,22 @@ import random
 from bubbleSort import bubble_sort
 from quicksort import quick_sort
 from mergesort import merge_sort
+from insertionsort import insertion_sort
+from heapsort import heap_sort
+from selectionsort import selection_sort
+
 
 root = Tk()
 root.title('Sorting Algorithm Visualisation')
 root.maxsize(900, 600)
 root.config(bg='black')
 
-#variables
+# variables
 selected_alg = StringVar()
 data = []
 
-#function
+
+# function
 def drawData(data, colorArray):
     canvas.delete("all")
     c_height = 380
@@ -22,18 +27,18 @@ def drawData(data, colorArray):
     x_width = c_width / (len(data) + 1)
     offset = 30
     spacing = 10
-    normalizedData = [ i / max(data) for i in data]
+    normalizedData = [i / max(data) for i in data]
     for i, height in enumerate(normalizedData):
-        #top left
+        # top left
         x0 = i * x_width + offset + spacing
         y0 = c_height - height * 340
-        #bottom right
+        # bottom right
         x1 = (i + 1) * x_width + offset
         y1 = c_height
 
         canvas.create_rectangle(x0, y0, x1, y1, fill=colorArray[i])
-        canvas.create_text(x0+2, y0, anchor=SW, text=str(data[i]))
-    
+        canvas.create_text(x0 + 2, y0, anchor=SW, text=str(data[i]))
+
     root.update_idletasks()
 
 
@@ -46,45 +51,53 @@ def Generate():
 
     data = []
     for _ in range(size):
-        data.append(random.randrange(minVal, maxVal+1))
+        data.append(random.randrange(minVal, maxVal + 1))
 
-    drawData(data, ['red' for x in range(len(data))]) #['red', 'red' ,....]
+    drawData(data, ['red' for x in range(len(data))])  # ['red', 'red' ,....]
+
 
 def StartAlgorithm():
     global data
     if not data: return
 
     if algMenu.get() == 'Quick Sort':
-        quick_sort(data, 0, len(data)-1, drawData, speedScale.get())
-    
+        quick_sort(data, 0, len(data) - 1, drawData, speedScale.get())
+
     elif algMenu.get() == 'Bubble Sort':
         bubble_sort(data, drawData, speedScale.get())
 
     elif algMenu.get() == 'Merge Sort':
         merge_sort(data, drawData, speedScale.get())
-    
+    elif algMenu.get() =='Selection Sort':
+        selection_sort(data, drawData, speedScale.get())
+    elif algMenu.get() == 'Heap sort':
+        heap_sort(data, drawData, speedScale.get())
+    elif algMenu.get() == 'Insertion Sort':
+        insertion_sort(data, drawData, speedScale.get())
+
     drawData(data, ['green' for x in range(len(data))])
 
 
-#frame / base lauout
-UI_frame = Frame(root, width= 600, height=200, bg='grey')
+# frame / base lauout
+UI_frame = Frame(root, width=600, height=200, bg='grey')
 UI_frame.grid(row=0, column=0, padx=10, pady=5)
 
 canvas = Canvas(root, width=600, height=380, bg='white')
 canvas.grid(row=1, column=0, padx=10, pady=5)
 
-#User Interface Area
-#Row[0]
+# User Interface Area
+# Row[0]
 Label(UI_frame, text="Algorithm: ", bg='grey').grid(row=0, column=0, padx=5, pady=5, sticky=W)
-algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=['Bubble Sort', 'Quick Sort', 'Merge Sort'])
+algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=['Bubble Sort', 'Quick Sort', 'Merge Sort','Selection Sort','Heap sort','Insertion Sort'])
 algMenu.grid(row=0, column=1, padx=5, pady=5)
 algMenu.current(0)
 
-speedScale = Scale(UI_frame, from_=0.1, to=5.0, length=200, digits=2, resolution=0.2, orient=HORIZONTAL, label="Select Speed [s]")
+speedScale = Scale(UI_frame, from_=0.1, to=5.0, length=200, digits=2, resolution=0.2, orient=HORIZONTAL,
+                   label="Select Speed [s]")
 speedScale.grid(row=0, column=2, padx=5, pady=5)
 Button(UI_frame, text="Start", command=StartAlgorithm, bg='red').grid(row=0, column=3, padx=5, pady=5)
 
-#Row[1]
+# Row[1]
 sizeEntry = Scale(UI_frame, from_=3, to=25, resolution=1, orient=HORIZONTAL, label="Data Size")
 sizeEntry.grid(row=1, column=0, padx=5, pady=5)
 
@@ -97,4 +110,5 @@ maxEntry.grid(row=1, column=2, padx=5, pady=5)
 Button(UI_frame, text="Generate", command=Generate, bg='white').grid(row=1, column=3, padx=5, pady=5)
 
 root.mainloop()
+
 
