4685af6422e6e8b6ca7aacdc2b0581c68e79f440
Arjun-Raghav <87092161+Arjun-Raghav@users.noreply.github.com>
2022-07-05 02:37:31 +0530

    Update heapsort.py

--- heapsort.py
+++ heapsort.py
@@ -1,5 +1,22 @@
 import time
 
+def to_heap(array, chosen_root, n, display, speed):
+    time.sleep(speed)
+    index = chosen_root
+    l = 2 * chosen_root + 1
+    r = 2 * chosen_root + 2
+
+    if l < n and array[index] < array[l]:
+        index = l
+    if r < n and array[index] < array[r]:
+        index = r
+    if index != chosen_root:
+        display(array,
+                ['blue' if x == chosen_root or x == index else 'green' if x >= n else 'red' for x in range(len(array))])
+        array[index], array[chosen_root] = array[chosen_root], array[index]
+        to_heap(array, index, n, display, speed)
+
+
 def heap_sort(data,drawData,timeTick):
     for index in range(len(data) // 2 - 1, -1, -1):
         to_heap(data, index, len(data),drawData,timeTick)
